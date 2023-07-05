1427b3666cc039c2cd6157035530927fc91e5a9d
AllokeyFS <atosh.rami@gmail.com>
2023-07-05 11:48:57 +0600

    first commit

---
+++ keyboards/kb.py
@@ -0,0 +1,9 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+
+def get_keyboard(name: str):
+    if name == 'start_menu':
+        keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(KeyboardButton('Товары'), ('Корзина'))
+        keyboard.add(KeyboardButton('Личный кабинет'), ('Тех.Поддержка'))
+    return keyboard
